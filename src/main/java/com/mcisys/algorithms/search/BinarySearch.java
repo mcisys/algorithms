ea12a03fa48bb48a768f0e95e3f9b04fbd383b2b
jack <410027018@qq.com>
2020-04-20 00:11:46 +0800

    add binary search

--- src/main/java/com/mcisys/algorithms/search/BinarySearch.java
+++ src/main/java/com/mcisys/algorithms/search/BinarySearch.java
@@ -1,4 +1,22 @@
 package main.java.com.mcisys.algorithms.search;
 
 public class BinarySearch {
+
+    public static boolean exist(int[] sortedArr, int num) {
+        if (sortedArr == null || sortedArr.length == 0) return false;
+        int l = 0;
+        int r = sortedArr.length - 1;
+        int mid = 0;
+        while (l < r) {
+            mid = l + ((r - l) >> 1); // mid = (l + r) / 2
+            if (sortedArr[mid] == num) {
+                return true;
+            } else if (sortedArr[mid] > num) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return sortedArr[l] == num;
+    }
 }
